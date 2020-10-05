e9997a0a168b81175b79c449c9d9bfbb9b0158fc
Mark Wagie <mark.wagie@tutanota.com>
2020-10-04 22:01:07 -0600

    Add kdialog title

--- zensu.sh
+++ zensu.sh
@@ -3,7 +3,7 @@ if [ -e /usr/bin/spacefm ]; then
 	eval "$(spacefm -g --label "Authentication required for $USER" --hbox --icon password --password --button cancel --button ok --title Authentication)"
 	PASSWD="$dialog_password1"
 elif [ -e /usr/bin/kdialog ]; then
-	PASSWD=$(kdialog --password "Authentication required for $USER")
+	PASSWD=$(kdialog --title "Authentication" --password "Authentication required for $USER")
 elif [ -e /usr/bin/yad ]; then
 	PASSWD="$(yad --entry --entry-label "Password" --hide-text --image=password --window-icon=dialog-password --text="Authentication required for $USER" --title=Authentication --center)"
 else
@@ -26,7 +26,7 @@ if [ $? = 1 ]; then
 		eval "`spacefm -g --label "Wrong password, retry" --hbox --icon dialog-error --password --window-icon=dialog-password --button cancel --button ok --title Authentication`"
 		PASSWD="$dialog_password1"
 	elif [ -e /usr/bin/kdialog ]; then
-		PASSWD=$(kdialog --password "Authentication required for $USER")
+		PASSWD=$(kdialog --title "Authentication" --password "Authentication required for $USER")
 	elif [ -e /usr/bin/yad ]; then
 		PASSWD="$(yad --entry --entry-label "Password" --hide-text --image=dialog-error --window-icon=dialog-password --text="Wrong password, retry" --title=Authentication --center)"
 	else
